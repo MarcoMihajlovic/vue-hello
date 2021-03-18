74c54c8abcfac8ccc2dd4e09d89d75291e0869a5
Marco <marcosinisa.mihajlovic@gmail.com>
2021-03-18 14:09:22 +0100

    Esercizio con Bonus

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var container = new Vue ({
+    el: '#container',
+
+    data: {
+        text: 'Messaggio per h1',
+
+        mySource: 'https://images.wired.it/wp-content/uploads/2020/03/20144558/42e8604d-375f-4e70-8716-24a9a830d2a8.gif'
+    }
+
+});
